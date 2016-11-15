5df415d2bf5ea064c6c062cf91d7e833a98219b3
Bartek <bskorka@sigma.ug.edu.pl>
2016-11-15 11:13:10 +0100

    eureka

--- src/main/java/pl/bskorka/ReservationServiceApplication.java
+++ src/main/java/pl/bskorka/ReservationServiceApplication.java
@@ -3,9 +3,11 @@ package pl.bskorka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
